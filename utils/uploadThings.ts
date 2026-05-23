dce228f9fe4c9ed15b3f4465227e7eda538d789b
nishant baiga <nishantbaiga@gmail.com>
2026-05-23 17:03:43 +0530

    refactor: remove unused API routes and components for PDF and message handling

--- utils/uploadThings.ts
+++
@@ -1,5 +0,0 @@
-import { OurFileRouter } from "../app/(server)/api/uploadthing/core";
-import { generateReactHelpers } from "@uploadthing/react";
-
-
-export const { useUploadThing, uploadFiles } = generateReactHelpers< OurFileRouter >();
